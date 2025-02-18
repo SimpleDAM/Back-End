3aeb4ebb3577861cb37175d60a07524fa9397f37
SimpleDAM <russ@daydream.co.uk>
2025-02-18 10:52:50 +0000

    Updated copyright date

--- src/includes/classes/api.settings.php
+++ src/includes/classes/api.settings.php
@@ -2,7 +2,7 @@
 /*
 This software is released under the BSD-3-Clause License
 
-Copyright 2022 Daydream Interactive Limited
+Copyright 2025 Daydream Interactive Limited
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
 
